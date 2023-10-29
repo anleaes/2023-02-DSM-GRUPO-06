ee1f24be791d179ae8e99fb063309b7ab230290a
thelogann <146785990+thelogann@users.noreply.github.com>
2023-10-29 02:09:08 -0300

    Create socialnetwork.py

---
+++ socialnetwork.py
@@ -0,0 +1,11 @@
+# Classe SocialNetwork
+class SocialNetwork:
+    def __init__(self, nome, descrição):
+        self._nome = nome
+        self._descrição = descrição
+        
+# Classe Client_SocialNetwork com herança do Client e do SocialNetwork
+class Client_SocialNetwork(Cliente, SocialNetwork):
+    def __init__(self,primeiro_nome, ultimo_nome, endereco, numero_telefone, email, genero, nome, descrição):
+      return Client()
+      return SocialNetwork()
