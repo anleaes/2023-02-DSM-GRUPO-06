6817d74d6f5845858bdb33315c31eea4673316e9
Gabriel Amaral <gabriel.amaral414@hotmail.com>
2023-11-13 21:29:35 -0300

    dasdasd asd asd asd ds

--- projetoapp/urls.py
+++ projetoapp/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('categorias/', include('categories.urls', namespace='categories')),
+    path('produtos/', include('products.urls', namespace='products')),
 ]
