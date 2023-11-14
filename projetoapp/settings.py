8558486f46084a0f3aa0847e91866754c866d98d
j0a0porto <joaopedroalvesporto0510@gmail.com>
2023-11-13 21:33:35 -0300

    adadaad

--- projetoapp/settings.py
+++ projetoapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'categories.apps.CategoriesConfig',
+    'socialnetworks.apps.SocialnetworksConfig',
 ]
 
 MIDDLEWARE = [
