d2c98594e7649e22f64606406f7516c95c2f0e1c
j0a0porto <joaopedroalvesporto0510@gmail.com>
2023-11-30 20:46:43 -0300

    Branch codigo funcionando

--- a3projeto/apps/usuarios/views.py
+++ a3projeto/apps/usuarios/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .models import Usuarios
+from .models import Usuario
 from rest_framework import viewsets
 from .serializer import UsuarioSerializer
 
 # Create your views here.
 
 class UsuarioViewSet(viewsets.ModelViewSet):
-    queryset = Usuarios.objects.all()
+    queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer  
     
