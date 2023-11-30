67eca6130c3bea1651306a179808941dda29e622
j0a0porto <joaopedroalvesporto0510@gmail.com>
2023-11-30 19:52:16 -0300

    Branch Localizacao Url

--- a3projeto/projeto/urls.py
+++ a3projeto/projeto/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls', namespace='usuarios')),
+    path('localizacoes/', include('localizacoes.urls', namespace='localizacoes')),
 ]
