68b20965f497d50f2d3371a20e5609a3b4a08735
j0a0porto <joaopedroalvesporto0510@gmail.com>
2023-11-30 20:01:38 -0300

    Branch Localizacao Serializer

---
+++ a3projeto/apps/localizacoes/serializer.py
@@ -0,0 +1,7 @@
+from .models import Localizacao
+from rest_framework import serializers
+
+class LocalizacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Localizacao
+        fields = '__all__'
