747c82e59d1d8d45b2df7a2cfc9bc3c301228bed
thelogann <146785990+thelogann@users.noreply.github.com>
2023-12-01 20:03:11 -0300

    Update views.py

--- a3projeto/apps/fornecedores/views.py
+++ a3projeto/apps/fornecedores/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Solicitacao
+from .models import Fornecedor
 from rest_framework import viewsets
 from .serializer import FornecedorSerializer
 
