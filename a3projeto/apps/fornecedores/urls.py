8a445fff14ab67dc59a61a6004095cc96140a2d2
thelogann <146785990+thelogann@users.noreply.github.com>
2023-12-01 20:23:24 -0300

    Add files via upload

---
+++ a3projeto/apps/fornecedores/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+app_name = 'fornecedores'
+
+router = routers.DefaultRouter()
+router.register('', views.SolicitacaoViewSet, basename='fornecedores')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
