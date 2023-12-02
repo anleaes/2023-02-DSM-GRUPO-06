f990c44a7b045a4ffaf555a69a3c0902d1bc7779
j0a0porto <joaopedroalvesporto0510@gmail.com>
2023-12-02 11:35:04 -0300

    Branch Materiaisitens Views

--- a3projeto/apps/materiaisitens/views.py
+++ a3projeto/apps/materiaisitens/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Materiaisitens
+from rest_framework import viewsets
+from .serializer import MateriaisitensSerializer
 
 # Create your views here.
+class MateriaisitensViewSet(viewsets.ModelViewSet):
+    queryset = Materiaisitens.objects.all()
+    serializer_class = MateriaisitensSerializer  
