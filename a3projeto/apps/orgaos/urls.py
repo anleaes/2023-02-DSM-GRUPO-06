8214e02fb9d893074e0cbdff074df77e57a3f26a
j0a0porto <118741333+j0a0porto@users.noreply.github.com>
2023-12-02 08:19:19 -0300

    Update urls.py

--- a3projeto/apps/orgaos/urls.py
+++ a3projeto/apps/orgaos/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-app_name = 'orgãos'
+app_name = 'orgaos'
 
 router = routers.DefaultRouter()
-router.register('', views.OrgaoViewSet, basename='orgãos')
+router.register('', views.OrgaoViewSet, basename='orgaos')
 
 urlpatterns = [
     path('', include(router.urls) )
